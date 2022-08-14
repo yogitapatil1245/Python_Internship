234e7c4173613ac804d703a4908deb742312a6bb
yogitapatil1245 <yogitapatil9785@gmail.com>
2022-08-14 14:23:08 +0530

    list

---
+++ day7/list_2.py
@@ -0,0 +1,82 @@
+#uitlist=list(("grapes","banana","apple"))
+print(fruitlist)
+
+fruitlistwithoutusingconstructor=["grapes","banana","apple"]
+print(fruitlistwithoutusingconstructor)
+
+remixlist=list(("grapes","21.5","name","0"))
+print(remixlist)
+
+
+#changing the elements
+
+fruitlist[0]="strawberry"list using constructor
+
+fr
+print(fruitlist)
+
+newfruitlist=["apple", "cherry", "orange", "kiwi", "mango"]
+print(newfruitlist)
+
+newfruitlist[1:3]=["banana","grapes","strawberry"]
+print(newfruitlist)
+
+newfruitlist=["orange", "kiwi", "mango"]
+newfruitlist[1:3]=["apple"]
+print(newfruitlist)
+
+
+#using insert() method
+newfruitlist.insert(2,"banana")
+print(newfruitlist)
+
+#using append() method to insert element at the end of the list
+newfruitlist.append("kiwi")
+print(newfruitlist)
+
+#using extend() method
+extendlist=newfruitlist.extend(fruitlist)
+print(extendlist)
+
+list1=["orange", "kiwi", "mango"]
+list2=["grapes","banana","apple"]
+tuple1=["grapes","banana","apple"]
+
+list1.extend(list2)
+print(list1)
+list1.extend(tuple1)
+print(list1)
+
+#using remove() method
+list1.remove("grapes")
+print(list1)
+
+#using pop() method
+list1.pop(2)
+print(list1)
+
+list1.pop()
+print(list1)
+
+#using Del keyword 
+del list1[0]
+print(list1)
+
+#delete complete list
+del list1
+#print(list1)
+
+del tuple1
+#print(tuple1)sssssssssss
+
+#empty the list using clear() method
+print("list2=",list2)
+list2.clear()
+print("list2=",list2)
+
+print(len(list2))
+#list2[0="orange"]
+#print(list2)
+list2.insert(0,"grapes")
+print(list2)
+
